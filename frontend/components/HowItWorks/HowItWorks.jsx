5ddc6498cdf51fb7c601dfe8133a80b7b1746ee3
AliErcanOzgokce <aliercanozgokce@gmail.com>
2024-06-03 16:32:50 +0300

    How it works responsive design changes

--- frontend/components/HowItWorks/HowItWorks.jsx
+++ frontend/components/HowItWorks/HowItWorks.jsx
@@ -117,9 +117,9 @@ export default function HowItWorks() {
           </div>
         </li>
       </ul>
-      <ul className="timeline timeline-vertical xl:hidden max-md:w-5/6">
-        <li>
-          <div className="timeline-start mb-10 max-w-xs timeline-box border-primary flex flex-col gap-5 py-10 px-5 justify-center text-center">
+      <ul className="steps steps-vertical xl:hidden ">
+        <li className="step step-primary">
+          <div className=" mb-10 max-w-xs timeline-box border-primary flex flex-col gap-5 py-10 px-5 justify-center text-center">
             <div className="w-full flex justify-center">
               <Image src={wallet} height={80} />
             </div>
@@ -127,39 +127,9 @@ export default function HowItWorks() {
             Easily connect any supported wallet to our platform to start
             creating your token in minutes.
           </div>
-          <div className="timeline-middle">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              className="w-5 h-5 text-primary"
-            >
-              <path
-                fillRule="evenodd"
-                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </div>
-          <hr className="bg-primary" />
         </li>
-        <li>
-          <hr className="bg-primary" />
-          <div className="timeline-middle">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              className="w-5 h-5 text-primary"
-            >
-              <path
-                fillRule="evenodd"
-                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </div>
-          <div className="timeline-end mb-10 max-w-xs timeline-box border-primary flex flex-col gap-5 py-10 px-5 justify-center text-center">
+        <li className="step step-primary">
+          <div className=" mb-10 max-w-xs timeline-box border-primary flex flex-col gap-5 py-10 px-5 justify-center text-center">
             <div className="w-full flex justify-center">
               <Image src={token} height={80} />
             </div>
@@ -167,59 +137,23 @@ export default function HowItWorks() {
             Provide essential details for your token, including its name,
             symbol, decimals, and total supply.
           </div>
-          <hr className="bg-primary" />
         </li>
-        <li>
-          <hr className="bg-primary" />
-          <div className="timeline-start mb-10 max-w-xs timeline-box border-primary flex flex-col gap-5 py-10 px-5 justify-center text-center">
+        <li className="step"><div className="mb-10 max-w-xs timeline-box border-primary flex flex-col gap-5 py-10 px-5 justify-center text-center">
             <div className="w-full flex justify-center">
               <Image src={choice} height={80} />
             </div>
             <div className="text-lg font-black">Choose Additional Features</div>
             Customize your token by selecting from a wide range of features like
             minting, burning, taxes, and more.
-          </div>
-          <div className="timeline-middle">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              className="w-5 h-5 text-primary"
-            >
-              <path
-                fillRule="evenodd"
-                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </div>
-          <hr />
-        </li>
-        <li>
-          <hr />
-          <div className="timeline-middle">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              className="w-5 h-5"
-            >
-              <path
-                fillRule="evenodd"
-                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </div>
-          <div className="timeline-end mb-10 max-w-xs timeline-box border-primary flex flex-col gap-5 py-10 px-5 justify-center text-center">
+          </div></li>
+        <li className="step"><div className="mb-10 max-w-xs timeline-box border-primary flex flex-col gap-5 py-10 px-5 justify-center text-center">
             <div className="w-full flex justify-center">
               <Image src={deploy} height={80} />
             </div>
             <div className="text-lg font-black">Deploy Your Token</div>
             Deploy your token to the blockchain ecosystem, making it instantly
             available for use across the network.
-          </div>
-        </li>
+          </div></li>
       </ul>
     </section>
   );
