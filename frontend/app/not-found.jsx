47e2e39943dca1b9952e3f2abdeaa779f2120f70
AliErcanOzgokce <aliercanozgokce@gmail.com>
2024-06-03 20:53:04 +0300

    updated github logo and 404 page fix

--- frontend/app/not-found.jsx
+++ frontend/app/not-found.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function NotFound() {
   return (
-    <main className="min-h-screen flex flex-col items-center justify-center bg-base-100 gap-10 max-md:min-h-96 max-md:py-80">
+    <main className="min-h-screen flex flex-col items-center justify-center bg-base-100 max-md:px-5 max-sm:text-center gap-10 max-md:min-h-96 max-md:py-80">
     <div className="flex flex-col ">
       <h1 className="text-primary text-4xl max-sm:text-3xl font-bold">404</h1>
       <h1 className='text-6xl max-sm:text-5xl font-extrabold'>Page Not Found!</h1>
