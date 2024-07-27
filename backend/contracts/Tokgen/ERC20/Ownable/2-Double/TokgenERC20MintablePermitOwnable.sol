714a09055e83edb37f48ea47eed67e30e7c0c959
AliErcanOzgokce <aliercanozgokce@gmail.com>
2024-07-27 21:34:18 +0300

    Creating double variation contracts

---
+++ backend/contracts/Tokgen/ERC20/Ownable/2-Double/TokgenERC20MintablePermitOwnable.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+
+contract TokgenERC20MintablePermitOwnable is ERC20, Ownable, ERC20Permit {
+    constructor(
+        address initialOwner,
+        string memory name,
+        string memory symbol,
+        uint8 premint
+    ) ERC20(name, symbol) Ownable(initialOwner) ERC20Permit(name) {
+        _mint(msg.sender, premint * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
---
+++ backend/contracts/Tokgen/ERC20/Ownable/2-Double/TokgenERC20PausableFlashMintOwnable.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20FlashMint.sol";
+
+contract TokgenERC20PausableFlashMintOwnable is
+    ERC20,
+    ERC20Pausable,
+    Ownable,
+    ERC20FlashMint
+{
+    constructor(
+        address initialOwner,
+        string memory name,
+        string memory symbol,
+        uint8 premint
+    ) ERC20(name, symbol) Ownable(initialOwner) {
+        _mint(msg.sender, premint * 10 ** decimals());
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _update(
+        address from,
+        address to,
+        uint256 value
+    ) internal override(ERC20, ERC20Pausable) {
+        super._update(from, to, value);
+    }
+}
