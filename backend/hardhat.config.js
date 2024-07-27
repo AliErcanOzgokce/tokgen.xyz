7bc780308a02a533868bb821450e20a247965546
AliErcanOzgokce <aliercanozgokce@gmail.com>
2024-07-27 21:13:14 +0300

    Testing to deploy smart contract to all chains

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -3,4 +3,74 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  networks: {
+    // INFURA
+    ethereum: { // Tested
+      url: "https://sepolia.infura.io/v3/c6f973393b1943528ef758d8a9e877ec",
+      accounts: [
+        "0x61b115a4e9e5c5906f53bb0150d80be67db5f598c1fc539517ec87c355004286",
+      ],
+    },
+    linea: { // Tested
+      url: "https://linea-sepolia.infura.io/v3/c6f973393b1943528ef758d8a9e877ec",
+      accounts: [
+        "0x61b115a4e9e5c5906f53bb0150d80be67db5f598c1fc539517ec87c355004286",
+      ],
+    },
+    avalanche: { // Tested
+      url: "https://avalanche-fuji.infura.io/v3/c6f973393b1943528ef758d8a9e877ec",
+      accounts: [
+        "0x61b115a4e9e5c5906f53bb0150d80be67db5f598c1fc539517ec87c355004286",
+      ],
+    },
+    polygon: { // Tested
+      url: "https://polygon-amoy.infura.io/v3/c6f973393b1943528ef758d8a9e877ec",
+      accounts: [
+        "0x61b115a4e9e5c5906f53bb0150d80be67db5f598c1fc539517ec87c355004286",
+      ],
+    },
+    blast: { // Tested
+      url: "https://blast-sepolia.infura.io/v3/c6f973393b1943528ef758d8a9e877ec",
+      accounts: [
+        "0x61b115a4e9e5c5906f53bb0150d80be67db5f598c1fc539517ec87c355004286",
+      ],
+    },
+    optimism: { // Tested
+      url: "https://optimism-sepolia.infura.io/v3/c6f973393b1943528ef758d8a9e877ec",
+      accounts: [
+        "0x61b115a4e9e5c5906f53bb0150d80be67db5f598c1fc539517ec87c355004286",
+      ],
+    },
+    arbitrum: { // Tested
+      url: "https://arbitrum-mainnet.infura.io/v3/c6f973393b1943528ef758d8a9e877ec",
+      accounts: [
+        "0x61b115a4e9e5c5906f53bb0150d80be67db5f598c1fc539517ec87c355004286",
+      ],
+    },
+    celo: { // Tested
+      url: "https://celo-alfajores.infura.io/v3/c6f973393b1943528ef758d8a9e877ec",
+      accounts: [
+        "0x61b115a4e9e5c5906f53bb0150d80be67db5f598c1fc539517ec87c355004286",
+      ],
+    },
+    // ALCHEMY
+    base: { // Tested
+      url: "https://base-sepolia.g.alchemy.com/v2/CsglrjoDtp4Zh507nMrgR8u-XXv3vb7W",
+      accounts: [
+        "0x61b115a4e9e5c5906f53bb0150d80be67db5f598c1fc539517ec87c355004286",
+      ],
+    },
+    fantom: { // Tested
+      url: "https://fantom-testnet.g.alchemy.com/v2/CsglrjoDtp4Zh507nMrgR8u-XXv3vb7W",
+      accounts: [
+        "0x61b115a4e9e5c5906f53bb0150d80be67db5f598c1fc539517ec87c355004286",
+      ],
+    },
+    zetachain: { // Tested
+      url: "https://zetachain-testnet.g.alchemy.com/v2/CsglrjoDtp4Zh507nMrgR8u-XXv3vb7W",
+      accounts: [
+        "0x61b115a4e9e5c5906f53bb0150d80be67db5f598c1fc539517ec87c355004286",
+      ],
+    }
+  },
 };
---
+++ backend/ignition/modules/Token.js
@@ -0,0 +1,31 @@
+const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
+
+/*
+ The callback passed to `buildModule()` provides a module builder object `m`
+ as a parameter. Through this builder object, you access the Module API.
+ For instance, you can deploy contracts via `m.contract()`.
+*/
+module.exports = buildModule("TokenModule", (m) => {
+  /*
+   Instead of named accounts, you get access to the configured accounts
+   through the `getAccount()` method.
+  */
+  const deployer = m.getAccount(0);
+  const tokenOwner = m.getAccount(1);
+
+  /*
+   Deploy `Token` by calling `contract()` with the constructor arguments
+   as the second argument. The account to use for the deployment transaction
+   is set through `from` in the third argument, which is an options object.
+  */
+  const token = m.contract("TokgenERC20Ownable", ["0xf1bb52640B43c157Bb429EB4d1F0F7Ae4e58f427", "MyToks", "sdf", 19]);
+
+  /*
+   The call to `m.contract()` returns a future that can be used in other `m.contract()`
+   calls (e.g. as a constructor argument, where the future will resolve to the
+   deployed address), but it can also be returned from the module. Contract
+   futures that are returned from the module can be leveraged in Hardhat tests
+   and scripts, as will be shown later.
+  */
+  return { token };
+});
