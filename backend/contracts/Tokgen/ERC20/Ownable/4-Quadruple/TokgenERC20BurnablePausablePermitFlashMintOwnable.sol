e3bb8e770eaa4580020aae6f1509de03d2ab0bcb
AliErcanOzgokce <aliercanozgokce@gmail.com>
2024-07-27 21:56:25 +0300

    Creating 4 feature variation contracts

---
+++ backend/contracts/Tokgen/ERC20/Ownable/4-Quadruple/TokgenERC20BurnablePausablePermitFlashMintOwnable.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20FlashMint.sol";
+
+contract TokgenERC20BurnablePausablePermitFlashMintOwnable is
+    ERC20,
+    ERC20Burnable,
+    ERC20Pausable,
+    Ownable,
+    ERC20Permit,
+    ERC20FlashMint
+{
+    constructor(
+        address initialOwner,
+        string memory name,
+        string memory symbol,
+        uint8 premint
+    ) ERC20(name, symbol) Ownable(initialOwner) ERC20Permit(name) {
+        _mint(msg.sender, premint * 10 ** decimals());
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _update(
+        address from,
+        address to,
+        uint256 value
+    ) internal override(ERC20, ERC20Pausable) {
+        super._update(from, to, value);
+    }
+}
---
+++ backend/contracts/Tokgen/ERC20/Ownable/4-Quadruple/TokgenERC20MintablePausablePermitFlashMintOwnable.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20FlashMint.sol";
+
+contract TokgenERC20MintablePausablePermitFlashMintOwnable is
+    ERC20,
+    ERC20Pausable,
+    Ownable,
+    ERC20Permit,
+    ERC20FlashMint
+{
+    constructor(
+        address initialOwner,
+        string memory name,
+        string memory symbol,
+        uint8 premint
+    ) ERC20(name, symbol) Ownable(initialOwner) ERC20Permit(name) {
+        _mint(msg.sender, premint * 10 ** decimals());
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _update(
+        address from,
+        address to,
+        uint256 value
+    ) internal override(ERC20, ERC20Pausable) {
+        super._update(from, to, value);
+    }
+}
